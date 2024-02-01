8f4f54eb305edeb93248c5d8b8bc3a087fda13fa
Joy Brar <joybrar2001@gmail.com>
2024-02-01 10:26:24 +0530

    Finished the home screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,41 @@
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+
+import useCurrentUser from "@/hooks/useCurrentUser";
+import Navbar from "@/components/Navbar";
+import BillBoard from "@/components/BillBoard";
+import MovieList from "@/components/MovieList";
+import useMovieList from "@/hooks/useMovieList";
+
+export async function getServerSideProps(context: NextPageContext){
+  const session = await getSession(context);
+
+  if(!session){
+    return{
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
+
 export default function Page() {
+  // const { data: user } = useCurrentUser();
+
+  const { data: movies = [] } = useMovieList()
+
   return (
     <>
-      <h1 className="text-2xl text-greem-500 underline ">Netflix Clone</h1>
+      <Navbar />
+      <BillBoard />
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies} />
+      </div>
     </>
   );
 }
